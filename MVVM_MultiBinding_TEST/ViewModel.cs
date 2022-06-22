6474f16f5c63b918b62ea1a791315c6bcff03672
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-22 11:26:08 +0200

    Created 'ICommand' property in 'ViewModel' class

--- MVVM_MultiBinding_TEST/ViewModel.cs
+++ MVVM_MultiBinding_TEST/ViewModel.cs
@@ -4,11 +4,13 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MVVM_MultiBinding_TEST
 {
     public class ViewModel : INotifyPropertyChanged
     {
+        public ICommand MyCommand { get; set; }
         public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
